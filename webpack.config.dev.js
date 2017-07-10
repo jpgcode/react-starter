44aeaed4e13463998723f649e64c4c80de06e7a8
Jose Pablo Granados <jpgcodecr@gmail.com>
2017-07-10 14:09:28 -0600

    Initial commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+    entry: './app/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'index.js'
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+        ]
+    },
+    devtool: 'source-map',
+    plugins: [HtmlWebpackPluginConfig]
+}
